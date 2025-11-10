deaedab4a30ab9a41d48f400e9c6ef3f0582f130
Shree Kumar Bauri <shribauri8083678211@gmail.com>
2025-11-10 18:35:58 +0530

    A simple Java Swing and MySQL-based Student Management System that allows users to search, view, and delete student records using a graphical user interface.
    This project is a mini Student management system built using Java Swing for the frontend and MySQL for the backend.
    It enables basic CRUD operations such as searching, viewing, and deleting student records from a connected MySQL database.
    It’s designed as a learning project to understand JDBC (Java Database Connectivity) and GUI-based database integration.

---
+++ src/main/java/com/mycompany/dbms/Dbms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dbms;
+
+/**
+ *
+ * @author shrib
+ */
+public class Dbms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
